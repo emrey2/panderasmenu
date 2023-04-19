5134df2312f9969989deab04ec3a2be509ba1b30
emrey2 <emrewp2@gmail.com>
2023-04-19 19:15:54 +0300

    something

--- modal.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".close").addEventListener("click", function () {
-  document.querySelector(".container").style.display = "none";
-});
-
-window.addEventListener("load", async function () {
-  setTimeout(function open(event) {
-    document.querySelector(".container").style.display = "block";
-  }, 2000);
-});
